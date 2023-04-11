a9fed55e56bae1c41964694e76eac91ffe09b2e0
martin0123478 <martin_agui97@outlook.es>
2023-04-11 13:27:57 -0600

    feat:validate email exist and acount confirm

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -19,6 +19,20 @@ const registrar = async (req, resp) => {
   }
 };
 
-const autenticar = async (req, res) => {};
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+  //Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+  //Comprobar si el usuario esta confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido cofirmada");
+    return res.status(403).json({ msg: error.message });
+  }
+  //Comprobar su password
+};
 
 export { registrar, autenticar };
