a555d99dcf9e0762f60b06cd20f8d8dbc8b79271
martin0123478 <martin_agui97@outlook.es>
2023-04-11 12:24:52 -0600

    feat:prevent duplicate users

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,12 +1,20 @@
 import Usuario from "../models/Usuario.js";
 
 const registrar = async (req, resp) => {
+  //Evitar registros duplicados
+
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email });
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return resp.status(400).json({ msg: error.message });
+  }
   try {
     const usuario = new Usuario(req.body);
     const usuarioAlmacenado = await usuario.save();
     resp.json(usuarioAlmacenado);
   } catch (error) {
-    console.log(usuario);
+    console.log(error);
   }
 };
 
