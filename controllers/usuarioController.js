21bcb1405decd4ab7efc8602da25e5bbc90ae3fe
martin0123478 <martin_agui97@outlook.es>
2023-04-11 12:11:37 -0600

    feat:create users in database

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,2 +1,13 @@
-const registrar = (req, resp) => {};
+import Usuario from "../models/Usuario.js";
+
+const registrar = async (req, resp) => {
+  try {
+    const usuario = new Usuario(req.body);
+    const usuarioAlmacenado = await usuario.save();
+    resp.json(usuarioAlmacenado);
+  } catch (error) {
+    console.log(usuario);
+  }
+};
+
 export { registrar };
