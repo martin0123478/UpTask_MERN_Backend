f066b7e4e861195406453ac2e474fadc01b47e9c
martin0123478 <martin_agui97@outlook.es>
2023-04-12 09:51:21 -0600

    feat:review if the user is auth

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,6 +1,28 @@
-const checkAuth = (req, res, next) => {
-  console.log("desde chaeckauth.js");
-  next();
+import jwt from "jsonwebtoken";
+import Usuario from "../models/Usuario.js";
+
+const checkAuth = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.usuario = await Usuario.findById(decoded.id).select(
+        "-password -confirmado -token -__v"
+      );
+      console.log(req.usuario);
+    } catch (error) {
+      return res.status(404).json({ msg: "Hubo un error" });
+    }
+  }
+  if (!token) {
+    const error = new Error("Token no valido");
+    res.status(401).json({ msg: error.message });
+  }
+  return next();
 };
 
 export default checkAuth;
