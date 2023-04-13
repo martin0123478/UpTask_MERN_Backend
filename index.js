c41caae0c527ad212e4a505da55062f8a0c8c690
martin0123478 <martin_agui97@outlook.es>
2023-04-13 13:58:09 -0600

    feat:create routes and controllers of tasks

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 import usuariosRoutes from "./routes/usuariosRoutes.js";
 import proyectoRoutes from "./routes/proyectoRoutes.js";
+import tareaRoutes from "./routes/tareaRoutes";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ conectarDB();
 //Routing
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/proyectos", proyectoRoutes);
+app.use("/api/tareas", tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
