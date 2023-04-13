540a8de948cc9654cd08819e08aa7c39dbdc292a
martin0123478 <martin_agui97@outlook.es>
2023-04-13 17:43:28 -0600

    feat:update data of tasks

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,3 +1,4 @@
+import { json } from "express";
 import Proyecto from "../models/Proyecto.js";
 import Tarea from "../models/Tareas.js";
 
@@ -31,7 +32,27 @@ const obtenerTarea = async (req, res) => {
   res.json(tarea);
 };
 
-const actualizarTarea = async (req, res) => {};
+const actualizarTarea = async (req, res) => {
+  const { id } = req.params;
+  const tarea = await Tarea.findById(id).populate("proyecto");
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no permitida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  tarea.nombre = req.body.nombre || tarea.nombre;
+  tarea.descripcion = req.body.descripcion || tarea.descripcion;
+  tarea.prioridad = req.body.prioridad || tarea.prioridad;
+  tarea.prioridad = req.body.prioridad || tarea.prioridad;
+  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+
+  try {
+    const tareaAlmacenada = await tarea.save();
+    res.json(tareaAlmacenada);
+  } catch (error) {
+    console.log(error);
+  }
+};
 const eliminarTarea = async (req, res) => {};
 const cambiarEstado = async (req, res) => {};
 
