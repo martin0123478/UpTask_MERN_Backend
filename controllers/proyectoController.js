1f93fbfe06a2bf2e145b2962e6355726f5f805e1
martin0123478 <martin_agui97@outlook.es>
2023-05-11 13:56:01 -0600

    feat:Delete collaborator

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -21,7 +21,9 @@ const nuevoProyecto = async (req, res) => {
 
 const obtenerProyecto = async (req, res) => {
   const { id } = req.params;
-  const proyecto = await Proyecto.findById(id).populate("tareas");
+  const proyecto = await Proyecto.findById(id)
+    .populate("tareas")
+    .populate("colaboradores", "nombre email");
 
   console.log();
   if (proyecto.creador.toString() !== req.usuario._id.toString()) {
@@ -116,7 +118,22 @@ const agregarColaborador = async (req, res) => {
   res.json({ msg: "Colaborador agregado correctamente" });
 };
 
-const eliminarColaborador = async (req, res) => {};
+const eliminarColaborador = async (req, res) => {
+  const proyecto = await Proyecto.findById(req.params.id);
+  if (!proyecto) {
+    const error = new Error("Proyecto no encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  proyecto.colaboradores.pull(req.body.id);
+
+  await proyecto.save();
+  res.json({ msg: "Colaborador eliminado correctamente" });
+};
 
 export {
   obtenerProyectos,
