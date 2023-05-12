8afa89b3fc9f865458cd88b456b08d27fc046fa5
martin0123478 <martin_agui97@outlook.es>
2023-05-11 21:15:42 -0600

    feat:validate no collaboratos and alerts

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -2,7 +2,12 @@ import Proyecto from "../models/Proyecto.js";
 import Tareas from "../models/Tareas.js";
 import Usuario from "../models/Usuario.js";
 const obtenerProyectos = async (req, res) => {
-  const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
+  const proyectos = await Proyecto.find({
+    $or: [
+      { colaboradores: { $in: req.usuario } },
+      { creador: { $in: req.usuario } },
+    ],
+  }).select("-tareas");
 
   res.json(proyectos);
 };
@@ -26,7 +31,12 @@ const obtenerProyecto = async (req, res) => {
     .populate("colaboradores", "nombre email");
 
   console.log();
-  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+  if (
+    proyecto.creador.toString() !== req.usuario._id.toString() &&
+    !proyecto.colaboradores.some(
+      (colaborador) => colaborador._id.toString() === req.usuario._id.toString()
+    )
+  ) {
     const error = new Error("Accion no valida");
     return res.status(404).json({ msg: error.message });
   }
