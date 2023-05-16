552ac6fb8de9ee77f1744e74b2903f1a77f1360b
martin0123478 <martin_agui97@outlook.es>
2023-05-15 23:13:37 -0600

    feat:add tasks with socket io

--- index.js
+++ index.js
@@ -54,4 +54,9 @@ io.on("connection", (socket) => {
   socket.on("abrir proyecto", (proyecto) => {
     socket.join(proyecto);
   });
+
+  socket.on("nueva tarea", (tarea) => {
+    const proyecto = tarea.proyecto;
+    socket.to(proyecto).emit("tarea agragada", tarea);
+  });
 });
